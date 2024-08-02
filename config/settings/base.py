0c7329c0ac904cd8f5fc54c8f9d9b8ef75d3d23d
Hiroyuki Nakano <hnakano1214@gmail.com>
2024-08-02 21:56:53 +0900

    不要な情報を削除

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,15 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qxlrh4d+cotmpsgzw+kc@8@h_8#ar9(a@6q_sbv%%_h0u-$$z)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
